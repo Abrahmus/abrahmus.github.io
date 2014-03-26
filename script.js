0cf495c19efab20a337790ae0bfc11457f186c94
Igor Soarez <igorsoarez@gmail.com>
2014-03-26 13:59:46 +0000

    alternative

--- script.js
+++ script.js
@@ -1,129 +1,103 @@
 
 $(document).ready(function(){
-
-	var bubbleContainer = $('#body');
-	
-	var f = new Fountain(400, 150, bubbleContainer);
-
-	f.IsFixedPoint(false);
-		
-	f.start();
-
+  var bubbleContainer = $('#body');
+  var f = new Fountain(900, 100, bubbleContainer);
+  f.IsFixedPoint(false);
+  f.start();
 });
 
 var Fountain = function(horizontalPosition, nBubbles, container){
-	
-	var horizontalPosition = horizontalPosition;
+  var fixedPoint = true;
 
-	var nBubbles = nBubbles;
+  this.IsFixedPoint = function(fp){ fixedPoint = fp; }
 
-	var container = container;
+  this.start = function(){
+    this.interval = setInterval(generateBubble, 150);
+  }
 
-	var fixedPoint = true;
+  var pool = [];
+  for (var i=0; i<nBubbles; ++i)
+    pool.push($('<div />', {class:'bubble'}));
 
-	this.IsFixedPoint = function(fp){
+  function generateBubble(){
+    var element = pool.shift();
+    pool.push(element);
 
-		fixedPoint = fp;
-	}
+    var viewPort = getViewport();
+    container.append(element);
 
-	this.start = function(){
+    // generate offset
+    if(fixedPoint){
+      element.css('left', horizontalPosition + 'px');
+    } else {
+      var offset = getRandomNumber(0, viewPort[0]);
+      element.css('left', offset + 'px');
+    }
+    
+    // generate scale
+    var transform = getRandomNumber(40, 60);
+    // generate opacity
+    var opacity = getRandomNumber(10, 50)/100;
+    // animation
+    var moveClouds = getRandomNumber(10, 30);
+    var sideways = getRandomNumber(1, 5);
+    var animationValue = 'moveclouds ' + moveClouds + 's ease infinite, sideWays ' + sideways + 's ease-in-out infinite alternate';
 
-		generateBubbles(nBubbles);
-	}
+    var size = getRandomNumber(5, 20) + 'px';
+    element.css({
+      '-webkit-transform': 'scale(' + transform/100 + ')',
+      '-moz-transform': 'scale(' + transform/100 + ')',
+      'transform': 'scale(' + transform/100 + ')',
+      'opacity': (transform - 30)/100,
 
-	var generateBubbles = function(bubbles){
+      'width': size,
+      'height': size,
 
-		var remainingBubbles = bubbles;
-
-		if(remainingBubbles < 1){
-
-			return;
-		}
-
-		generateBubble();
-
-		setTimeout(function(){
-
-			generateBubbles(remainingBubbles - 1);
-
-		}, 500);
-	}
-
-	var generateBubble = function(){
-
-		var viewPort = getViewport();
-
-		var element = $('<div />', {class:'bubble'});
-
-		container.append(element);
-
-		// generate offset
-		if(fixedPoint){
-			element.css('left', horizontalPosition + 'px');
-		} else {
-			var offset = getRandomNumber(0, viewPort[0]);
-			element.css('left', offset + 'px');
-		}
-		
-		// generate scale
-		var transform = getRandomNumber(40, 60);
-		// generate opacity
-		var opacity = getRandomNumber(10, 50)/100;
-		// animation
-		var moveClouds = getRandomNumber(5, 10);
-		var sideways = getRandomNumber(1, 5);
-		var animationValue = 'moveclouds ' + moveClouds + 's ease infinite, sideWays ' + sideways + 's ease-in-out infinite alternate';
-
-		element.css({
-			'-webkit-transform': 'scale(' + transform/100 + ')',
-			'-moz-transform': 'scale(' + transform/100 + ')',
-			'transform': 'scale(' + transform/100 + ')',
-			'opacity': (transform - 30)/100,
-
-			'-webkit-animation': animationValue,
-			'-moz-animation': animationValue,
-			'-o-animation': animationValue
-		});
-	}
+      '-webkit-animation': animationValue,
+      '-moz-animation': animationValue,
+      '-o-animation': animationValue
+    });
+  }
 }
 
 function getViewport() {
 
-	var viewPortWidth;
-	var viewPortHeight;
+  var viewPortWidth;
+  var viewPortHeight;
 
-	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-	if (typeof window.innerWidth != 'undefined') {
-		viewPortWidth = window.innerWidth,
-		viewPortHeight = window.innerHeight
-	}
+  // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+  if (typeof window.innerWidth != 'undefined') {
+    viewPortWidth = window.innerWidth,
+    viewPortHeight = window.innerHeight
+  }
 
-	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-	else if (typeof document.documentElement != 'undefined'
-		&& typeof document.documentElement.clientWidth !=
-		'undefined' && document.documentElement.clientWidth != 0) {
-		viewPortWidth = document.documentElement.clientWidth,
-		viewPortHeight = document.documentElement.clientHeight
-	}
+  // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+  else if (typeof document.documentElement != 'undefined'
+    && typeof document.documentElement.clientWidth !=
+    'undefined' && document.documentElement.clientWidth != 0) {
+    viewPortWidth = document.documentElement.clientWidth,
+    viewPortHeight = document.documentElement.clientHeight
+  }
 
-	// older versions of IE
-	else {
-		viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
-		viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
-	}
+  // older versions of IE
+  else {
+    viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+    viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+  }
 
-	return [viewPortWidth, viewPortHeight];
+  return [viewPortWidth, viewPortHeight];
 }
 
 function getRandomNumber(minimum, maximum){
-	if(!maximum) {
-		max = 100;
-	}
+  if(!maximum) {
+    max = 100;
+  }
 
-	if(!minimum || minimum < 1)
-	{
-		minimum = 1;
-	}
+  if(!minimum || minimum < 1)
+  {
+    minimum = 1;
+  }
 
-	return (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum);
+  return (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum);
 }
+
